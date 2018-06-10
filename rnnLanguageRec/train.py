216234a439249e866299699fc6ebe941812cbef1
Venkat <vgella@andrew.cmu.edu>
2018-06-10 19:24:41 -0400

    implement SGD as optimization mechanism

--- rnnLanguageRec/train.py
+++ rnnLanguageRec/train.py
@@ -10,25 +10,21 @@ criterion = nn.NLLLoss()
 learning_rate = .005
 current_loss = 0
 num_correct = 0
+optimizer = torch.optim.SGD(rnn.parameters(), lr = learning_rate)
 
 for i in range(num_epochs):
    category, name, name_tensor, category_tensor = random_example()
-   rnn.zero_grad()
+   optimizer.zero_grad()
 
    hidden =rnn.initHidden()
    for l in range(name_tensor.size()[0]):
       output, hidden = rnn(name_tensor[l], hidden)
    loss = criterion(output, category_tensor)
    loss.backward()
-   for p in rnn.parameters():
-      p.data.add_(-learning_rate, p.grad.data)
-
+   optimizer.step()
    current_loss += loss.item()  
    guess  = get_output(output)
    if category in guess: num_correct += 1
    if(i %5000): 
        print("%d: %f" % (i, num_correct/i),num_correct)
        print(guess, category)
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-
